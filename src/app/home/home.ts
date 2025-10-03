ac1ce39942539b6ffdb6788244344bfc5ad33cdf
Tomasz Gnyp <wirkod@gmail.com>
2025-10-03 16:30:21 +0200

    feat: Add home component

---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+    selector: 'app-home',
+    imports: [MatButtonModule],
+    templateUrl: './home.html',
+    styleUrls: ['./home.scss']
+})
+export class Home { }
