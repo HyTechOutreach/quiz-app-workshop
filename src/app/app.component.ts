9cb2a4758a9d7a1e22dd2673a3a0bc2ae0970135
DudaRobert <duda_robert@outlook.com>
2025-10-07 14:58:10 +0200

    feat: Add toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, ToolbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    protected readonly title = signal('quiz-app-workshop');
+    protected readonly title = signal('My Quiz App');
 }
