a961cac3346ed42c33599213d6decffec98f7243
Tomasz Gnyp <wirkod@gmail.com>
2025-10-10 10:17:44 +0200

    feat: add tasks in one view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
     { path: 'quiz', loadComponent: () => import('./quiz/quiz.component').then(m => m.QuizComponent) },
+    { path: 'all-questions', loadComponent: () => import('./all-questions-quiz/all-questions-quiz.component').then(m => m.AllQuestionsQuizComponent) },
     { path: 'results', loadComponent: () => import('./results/results.component').then(m => m.ResultsComponent) },
 ];
